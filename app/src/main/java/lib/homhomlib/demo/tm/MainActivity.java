eb9c3311571c69a335052844e54c09e947d0e0a9
HomHomLin <linhh90@163.com>
2016-05-11 11:22:31 +0800

    update code

--- app/src/main/java/lib/homhomlib/demo/tm/MainActivity.java
+++ app/src/main/java/lib/homhomlib/demo/tm/MainActivity.java
@@ -3,6 +3,7 @@ package lib.homhomlib.demo.tm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
@@ -11,14 +12,14 @@ import lib.homhomlib.tm.TouchMenuHelper;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private RelativeLayout relativeLayout;
+    private TouchMenuHelper mTouchMenuHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.btnToggle);
-        relativeLayout = (RelativeLayout) findViewById(R.id.root);
-        TouchMenuHelper.setTarget(button, View.inflate(button.getContext(), R.layout.menu, null));
+        mTouchMenuHelper = new TouchMenuHelper(this,button,View.inflate(button.getContext(), R.layout.menu, null), ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,null);
+        mTouchMenuHelper.update();
     }
 }
--- library/src/main/java/lib/homhomlib/tm/TouchMenuHelper.java
+++ library/src/main/java/lib/homhomlib/tm/TouchMenuHelper.java
@@ -2,6 +2,7 @@ package lib.homhomlib.tm;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.TouchDelegate;
@@ -12,45 +13,124 @@ import android.widget.PopupWindow;
 /**
  * Created by Linhh on 16/4/25.
  */
-public class TouchMenuHelper {
+public class TouchMenuHelper implements GestureDetector.OnGestureListener,View.OnTouchListener{
+    private GestureDetector mGestureDetector;
+    private View mView;
+    private View mMenuView;
+    private int mMenuWidth,mMenuHeight;
+    private View.OnTouchListener mOnTouchListener;
 
-    public static void setTarget(View view, final View menuView){
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Object obj = v.getTag(v.getId());
-                PopupWindow popupWindow;
+    private PopupWindow mPopupWindow;
 
-                int[] location = new int[2];
+//    public TouchMenuHelper(Context context){
+//        this(context,null,null,0,0);
+//    }
 
-                if (v != null) {
-                    v.getLocationOnScreen(location);
-                }
+    public TouchMenuHelper(Context context, View view, View menuView, int menuWidth, int menuHeight){
+        this(context,view,menuView,menuWidth,menuHeight,null);
+    }
 
-                if (obj == null) {
-                    popupWindow = new PopupWindow(menuView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                    popupWindow.setFocusable(true);
-                    popupWindow.setOutsideTouchable(true);
-                    popupWindow.setBackgroundDrawable(v.getContext().getResources().getDrawable(
-                            R.drawable.popwindow_bg));
-                    popupWindow.update();
-                    v.setTag(v.getId(), popupWindow);
-                } else {
-                    popupWindow = (PopupWindow) obj;
-                }
+    public TouchMenuHelper(Context context, View view, View menuView, int menuWidth, int menuHeight, View.OnTouchListener onTouchListener){
+        mGestureDetector = new GestureDetector(context, this);
+        mView = view;
+        mMenuView = menuView;
+        mMenuHeight = menuHeight;
+        mMenuWidth = menuWidth;
+        mOnTouchListener = onTouchListener;
+    }
 
-                if (null != popupWindow) {
-                    if (!popupWindow.isShowing()) {
+    public void update(){
+//        if(mView == null){
+//            throw new Exception("mView is null");
+//        }
+//
+//        if(mMenuView == null){
+//            throw new Exception("mMenuView is null");
+//        }
 
-                        popupWindow.showAtLocation(v, Gravity.LEFT | Gravity.TOP, (int)event.getRawX(), (int)event.getRawY());
-                    }
-//                    else {
-//                        popupWindow.dismiss();
-//                    }
-                }
-                return false;
-            }
-        });
+        mPopupWindow = new PopupWindow(mMenuView, mMenuWidth, mMenuHeight);
+        mPopupWindow.setFocusable(true);
+        mPopupWindow.setOutsideTouchable(true);
+        mPopupWindow.setBackgroundDrawable(mView.getContext().getResources().getDrawable(
+                R.drawable.popwindow_bg));
+        mPopupWindow.update();
 
+        mView.setOnTouchListener(this);
+
+    }
+
+    public void setOnTouchListener(View.OnTouchListener listener){
+        mOnTouchListener = listener;
+    }
+
+    public void setTarget(View view){
+        mView = view;
+    }
+
+    public void setMenuView(View view){
+        mMenuView = view;
+    }
+
+    public void setMenuWidth(int menuWidth){
+        this.mMenuWidth = menuWidth;
+    }
+
+    public void setMenuHeight(int menuHeight){
+        this.mMenuHeight = menuHeight;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        if (mView == null || mMenuView == null) {
+            return;
+        }
+
+//        int[] location = new int[2];
+//        mView.getLocationOnScreen(location);
+
+//        if (mPopupWindow == null) {
+//            mPopupWindow = new PopupWindow(mMenuView, mMenuWidth, mMenuHeight);
+//            mPopupWindow.setFocusable(true);
+//            mPopupWindow.setOutsideTouchable(true);
+//            mPopupWindow.setBackgroundDrawable(mView.getContext().getResources().getDrawable(
+//                    R.drawable.popwindow_bg));
+//            mPopupWindow.update();
+//        }
+        if (!mPopupWindow.isShowing()) {
+            mPopupWindow.showAtLocation(mView, Gravity.LEFT | Gravity.TOP, (int)event.getRawX(), (int)event.getRawY());
+        }
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(mGestureDetector != null){
+            mGestureDetector.onTouchEvent(event);
+        }
+        return mOnTouchListener == null ? false : mOnTouchListener.onTouch(v,event);
     }
 }
